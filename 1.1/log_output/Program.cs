eb04a42b70680256d8f09ff4cf73dd8799906885
Remco Janssen <remcoj@xs4all.nl>
2025-10-12 11:41:09 +0800

    Ex1.1

---
+++ 1.1/log_output/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+ManualResetEvent canceled = new(false);
+
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    canceled.Set();
+};
+
+string randomStr = Guid.NewGuid().ToString();
+
+System.Timers.Timer timer = new(5000);
+
+DateTimeFormatInfo dtFormat = CultureInfo.InvariantCulture.DateTimeFormat;
+string dtFormatStr = dtFormat.UniversalSortableDateTimePattern;
+
+timer.Elapsed += (sender, e) =>
+    Console.WriteLine($"{DateTime.Now.ToString(dtFormatStr)}: {randomStr}");
+
+timer.Start();
+canceled.WaitOne();
+timer.Stop();
+canceled.Dispose();
