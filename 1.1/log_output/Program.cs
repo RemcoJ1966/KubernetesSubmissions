7bf8c8ad44151a5afd73bb1a8b2c151e7ef8b80e
Remco Janssen <remcoj@xs4all.nl>
2025-10-25 18:32:48 +0800

    Log-output minimal API

--- 1.1/log_output/Program.cs
+++ 1.1/log_output/Program.cs
@@ -1,24 +1,41 @@
 ﻿using System.Globalization;
 
-ManualResetEvent canceled = new(false);
+string? port = Environment.GetEnvironmentVariable("PORT");
 
-Console.CancelKeyPress += (sender, e) =>
+if (string.IsNullOrEmpty(port))
 {
-    e.Cancel = true;
-    canceled.Set();
-};
+    Console.WriteLine("Error: PORT environment variable is not set.");
+    Environment.Exit(-1); // Exit with error code -1
+}
 
-string randomStr = Guid.NewGuid().ToString();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-System.Timers.Timer timer = new(5000);
+builder.Services.AddSingleton<RandomStringService>();
+builder.Services.AddHostedService<RandomStringService>(sp => sp.GetRequiredService<RandomStringService>());
 
-DateTimeFormatInfo dtFormat = CultureInfo.InvariantCulture.DateTimeFormat;
-string dtFormatStr = dtFormat.UniversalSortableDateTimePattern;
+WebApplication app = builder.Build();
+app.Urls.Add($"http://0.0.0.0:{port}");
 
-timer.Elapsed += (sender, e) =>
-    Console.WriteLine($"{DateTime.Now.ToString(dtFormatStr)}: {randomStr}");
+app.MapGet("/", () => Results.Content($"<div><h1>Server started in port {port}</h1></div><div><p><a href=\"/status\">status</a></div>", "text/html"));
+app.MapGet("/status", (RandomStringService service) => Results.Content($"<div><h1>{service.Status}</h1></div><div><a href=\"/\">home</a></div>", "text/html"));
 
-timer.Start();
-canceled.WaitOne();
-timer.Stop();
-canceled.Dispose();
+Console.WriteLine($"Server started in port {port}");
+
+app.Run();
+
+public class RandomStringService : BackgroundService
+{
+    private static readonly string formatString =
+        CultureInfo.InvariantCulture.DateTimeFormat.UniversalSortableDateTimePattern;
+    private static readonly string randomStr = Guid.NewGuid().ToString();
+    public string Status => $"{DateTime.Now.ToString(formatString)}: {randomStr}";
+
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Console.WriteLine(Status);
+            await Task.Delay(5000, cancellationToken);
+        }
+    }
+}
