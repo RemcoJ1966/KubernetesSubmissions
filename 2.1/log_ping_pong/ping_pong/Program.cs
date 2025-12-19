e7b596584830cad803d6e8c7e12a064720d6983f
Remco Janssen <remcoj@xs4all.nl>
2025-12-19 18:25:55 +0800

    ex2.7

--- 2.1/log_ping_pong/ping_pong/Program.cs
+++ 2.1/log_ping_pong/ping_pong/Program.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+
 string? port = Environment.GetEnvironmentVariable("PORT");
 
 if (string.IsNullOrEmpty(port))
@@ -8,14 +10,44 @@ if (string.IsNullOrEmpty(port))
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.AddConsole();
+builder.Services.AddNpgsqlDataSource(
+    "Host=postgres-svc.default;Port=5432;Username=postgres;Password=example");
+
 WebApplication app = builder.Build();
 app.Urls.Add($"http://0.0.0.0:{port}");
 
 int count = 0;
 
-app.MapGet("/pingpong", () => $"pong {count++}");
+// Ensure the table exists
+NpgsqlDataSource? dataSource = app.Services.GetRequiredService<NpgsqlDataSource>();
+if (dataSource is null)
+{
+    Console.WriteLine("Error: datasource not registered.");
+    Environment.Exit(-1); // Exit with error code -1
+}
+
+await using NpgsqlCommand createTable = dataSource.CreateCommand(
+    "CREATE TABLE IF NOT EXISTS pings (ping_count INTEGER)");
+await createTable.ExecuteNonQueryAsync();
+
+
+// Endpoints
+app.MapGet("/pingpong", async (NpgsqlDataSource dataSource) => 
+{
+    count++;
+
+    await using NpgsqlCommand insert = dataSource.CreateCommand(
+        $"INSERT INTO pings (ping_count) VALUES ({count})");
+    await insert.ExecuteNonQueryAsync();
+
+    return $"pong {count}";
+});
+
 app.MapGet("/pings", () => $"{count}");
 
+
+// Run app
 Console.WriteLine($"Server started in port {port}");
 
 app.Run();
